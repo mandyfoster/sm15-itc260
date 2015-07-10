e820ac1af3b43d53273eb3d91e8ce12fb53a69de
mandyfoster <mandy.s.foster@gmail.com>
2015-07-09 18:17:33 -0700

    Adds News model and controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+//$route['news'] = 'news/view/$1';
+//$route['default_controller'] = 'pages/view';
+//$route['(:any)'] = 'pages/view/$1';
 
-$route['default_controller'] = 'pages/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
